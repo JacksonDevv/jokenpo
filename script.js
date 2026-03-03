dd87124933252dea2551ecff4ed6f4b67da62b22
Jacksondevv <cotchoco@hotmail.com>
2026-03-03 00:31:52 -0300

    jogo jokenpo

---
+++ script.js
@@ -0,0 +1,17 @@
+const aleatorio = document.getElementsByClassName("machine-score")
+
+    function escolhaMaquina() {
+ 
+        return Math.floor(Math.random() * 3);
+}
+
+
+const jogue = document.getElementsByClassName(".buttons button")
+
+function clicado() {
+    
+jogue.addEventListener("click")
+}
+
+alert("opaaaaa")
+
